77b7bf38d6f4f5699d0fbe2bf8b80bc3ac05152c
fao73Artem <fao_artem@mail.ru>
2023-04-05 14:34:00 +0300

    Add is_prime

--- homework_01/main.py
+++ homework_01/main.py
@@ -18,7 +18,18 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
-def is_prime():
+def is_prime(n):
+    if n<2:
+        return False
+    else:
+        for i in range(2, n+1):
+           if n % i == 0:
+               return False
+               break
+           else:
+               return True
+
+
 
 
 
